db5fb5161cdebc75565505b62da0cdab8d16c4c0
Dinadine <53227424+Dinadine@users.noreply.github.com>
2021-04-15 18:03:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const URL = 'https://pokeapi.co/api/v2/pokemon/';
+const URL2 = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/'
 
 function loadPokemon() {
     var id = document.getElementById('input-id');
     var pokemonName = document.getElementById('pokemon-name');
+    var pokemonImageCont = document.getElementById('pokemon-image');
 
     var opts = {
         method: 'GET',//POST,PUT,DELETE,OPTIONS,PATCH        
@@ -16,9 +18,16 @@ function loadPokemon() {
     })
     .then(function (body) {
         pokemonName.innerHTML = body.name;
+
+        var pokemonImage = new Image(200, 200);
+        pokemonImage.src= URL2 + id.value + '.png';
+        pokemonImageCont.appendChild(pokemonImage);
     });
+
+
 
 }
 
 var button = document.getElementById('submit-button');
 button.addEventListener('click', loadPokemon, true);
+
